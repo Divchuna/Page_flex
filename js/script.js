b15361cb39b35c70eb75765f5078e9c32bc82710
Ann <best.headwoman@gmail.com>
2022-09-29 12:19:18 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function func() {
+  document.getElementById('hero_btn').value  = 'Thank you!';
+}
+
+var swiper = new Swiper(".testi_swiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  
+  breakpoints: {
+      1200: {
+          slidesPerView: 4,
+          spaceBetween: 40
+      },
+      834: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+          
+      },
+      320: {
+          slidesPerView: 2,
+          spaceBetween: 24,
+          slidesOffsetBefore: 16,
+          slidesOffsetAfter: 32,
+          //centeredSlides: true,
+      }
+  },
+  
+  
+
+  navigation: {
+      nextEl: ".swiper-button-next-unique",
+      prevEl: ".swiper-button-prev-unique",
+    },
+});
+
